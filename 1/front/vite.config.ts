61f2b0836e1fc0b4747b9fca0ce178708e5d83be
Yionse <v-zhangtianc@microsoft.com>
2024-11-21 16:52:47 +0800

    :rocket: 开启gzip压缩

--- 1/front/vite.config.ts
+++ 1/front/vite.config.ts
@@ -1,29 +1,35 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { resolve } from "path"
+import compression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {},
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      ext: ".gz"
+    })
+  ],
   resolve: {
     alias: [
       // 配置 @ 指代 src
       {
         find: "@",
-        replacement: resolve(__dirname, "./src"),
+        replacement: resolve(__dirname, "./src")
       },
       {
         find: "@public",
-        replacement: resolve(__dirname, "./src/assets"),
-      },
-    ],
+        replacement: resolve(__dirname, "./src/assets")
+      }
+    ]
   },
   css: {
     preprocessorOptions: {
       less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
+        javascriptEnabled: true
+      }
+    }
+  }
 })
